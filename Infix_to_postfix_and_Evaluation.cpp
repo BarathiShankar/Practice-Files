6377d10b28078e72157d9fd32fec8646931b0ae7
Barathi Shankar <barathishankarr@gmail.com>
2025-09-07 20:10:55 +0530

    Created Infix_to_postfix_and_Evaluation.cpp
    Just began the code all the way

---
+++ Infix_to_postfix_and_Evaluation.cpp
@@ -0,0 +1,10 @@
+//Program to use stack to read a infix notation and convert ti to postfix and hence evaluate it
+#include<iostream>
+#include<stack>
+stack<char> s1;
+using namespace std;
+int main()
+{
+  return 0;
+}
+//UNDER DEVELOPEMENT
