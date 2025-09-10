6bc927adf99bfb65147d2274aa904995fc049cfd
Barathi Shankar <barathishankarr@gmail.com>
2025-09-10 15:25:42 +0530

    Create in_to_post_eval.cpp

---
+++ in_to_post_eval.cpp
@@ -0,0 +1,61 @@
+//Program to use stack to convert infix to postfix and hence evaluate it to get the final result ( Static sized stack )
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int prec(char op)//To check the precedence of operator
+{
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        if (op == '^') return 3;
+        return 0;
+}
+string intopost(const string& in)//Convert infix to postfix
+{       string post;
+        stack<char> s;
+        for(char c: in)
+        {
+                if(isdigit(c))
+                        post+=c;
+                else if(c=='(')
+                        s.push(c);
+                else if(c==')')
+                {
+                        while(!s.empty() && s.top()!='(')
+                        {
+                                post+=s.top();
+                                s.pop();
+                        }
+                        if(!s.empty()) s.pop(); //To pop out '(' braces
+                }
+                else
+                {       while((!s.empty()) && (prec(c) <= prec(s.top())))
+                        {
+                                post+=s.top();
+                                s.pop();
+                        }
+                s.push(c);
+                }
+        }
+        while(!s.empty())
+        {
+                post+=s.top();
+                s.pop();
+        }
+        return post;
+}
+void posteval(const string& post)//Evaluating the postfix expression
+{       double ans;
+        //cout<<ans;
+}
+int main()
+{
+        string in,post;
+        cout<<"Enter the infix expression : ";
+        getline(cin,in);
+        post=intopost(in);
+        cout<<"Converted postfix : "<<post<<endl;
+        posteval(post);
+        return 0;
+}
+//Pending to perform postfix evaluation
