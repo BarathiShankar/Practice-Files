3c5b8be2dbd603f8ca9005a98f0770feb6abc840
Barathi Shankar <barathishankarr@gmail.com>
2025-10-10 23:03:57 +0530

    Update java_event_prog1.java

--- java_event_prog1.java
+++ java_event_prog1.java
@@ -17,17 +17,13 @@ class app extends JFrame implements ActionListener
     JButton b1;
     app()
     {
-
-        setLayout(new GridLayout(4, 1)); // 4 rows, 1 column
         setLayout(new FlowLayout());
-        l2=new JLabel("Find Your Love Pair");
         l1=new JLabel("Enter Your Name : ");
         t1=new JTextField(20);
         b1=new JButton("Enter");
     add(l1);
     add(t1);
     add(b1);
-    add(l2);
     setVisible(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     b1.addActionListener(this);
@@ -36,6 +32,6 @@ class app extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         String A=t1.getText();
-        JOptionPane.showMessageDialog(null,"Dei "+A+" Unakku saavura vayasula love punda yaada... !","Love Tracker",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Mr/Mrs "+A+" How is my program","InfoBox",JOptionPane.INFORMATION_MESSAGE);
     }
 }
